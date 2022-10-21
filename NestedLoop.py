a36a27efc159dd68d1fb4f75d3be4d36199fb37b
Sumaiya Akter Shukhy <84353537+shukhy@users.noreply.github.com>
2022-10-21 22:55:43 +0600

    Add files via upload

---
+++ NestedLoop.py
@@ -0,0 +1,13 @@
+x = -100
+if x>0:
+    print('x is positive')
+    if (x%2)==0:
+        print('x is even')
+    else:
+        print('x is odd')
+else:
+    print('x is negative')
+    if (x % 2) == 0:
+        print('x is even')
+    else:
+        print('x is odd')
