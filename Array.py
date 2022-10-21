f9c4552df2efab667029386aeae9dcf4d27c7bfb
shukhy <84353537+shukhy@users.noreply.github.com>
2022-10-21 13:59:51 +0600

    Add files via upload

--- Array.py
+++ Array.py
@@ -1,1 +1,15 @@
+from array import *
 
+amount = array('i',[3555,2333,5666,7888,9666])
+print('print first amount')
+print(amount[0])
+print('All amount:')
+for i in range(5):
+   print(amount[i])
+print(amount.buffer_info())
+print(amount.reverse())
+print(amount)
+print(amount.append(4000))
+print(amount)
+print(amount.remove(2333))
+print(amount)
