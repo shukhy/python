9108b908eb6c09150d13c63a7fc190a2c52cc28f
Sumaiya Akter Shukhy <84353537+shukhy@users.noreply.github.com>
2022-10-23 13:30:37 +0600

    Add files via upload

---
+++ Lambda.py
@@ -0,0 +1,9 @@
+#by using lambda function
+Result = (lambda x,y,z : x * y * z)(2, 5, 9)
+print(Result)
+
+
+print('How many tools :')
+Tools = (lambda chair,table,bed:chair+table+bed)(10,23,7)
+print(Tools)
+
